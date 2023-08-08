711afd00dac4cf0cfaec0f228b7032900882aabc
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-08-08 12:41:50 +0300

    Building Calculator

---
+++ Calculator/index.ts
@@ -0,0 +1,11 @@
+import { question } from "readline-sync";
+
+function main(): void
+{
+    // void means, this function is notreturn anything
+    const firstStr: string = question("Enter first number:\n");
+    
+    
+}
+
+main();
