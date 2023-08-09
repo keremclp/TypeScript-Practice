1b8046967689c1b99e42dcefe121d2a95ca62a86
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2023-08-09 13:45:10 +0300

    tic-tac-toe added

---
+++ tic-tac-toe/src/components/Game.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+function Game(){
+    return (
+        <div>
+            <h1>Game</h1>
+        </div>
+    )
+}
+
+export default Game;
